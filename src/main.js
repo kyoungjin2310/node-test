f18ef3d9429944ffd3d2540caa2c6ad451de16a4
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-08-12 23:25:10 +0900

    feat: promise

--- src/main.js
+++ src/main.js
@@ -1,101 +1,31 @@
-// @ts-check
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable spaced-comment */
+/* @ts-check */
+
+/* eslint-disable no-new */
 /* eslint-disable no-console */
-const people = [
-  {
-    age: 20,
-    city: "서울",
-    pet: ["cat", "dog"],
-  },
-  {
-    age: 40,
-    city: "부산",
-  },
-  {
-    age: 31,
-    city: "대구",
-    pet: ["cat", "dog"],
-  },
-  {
-    age: 36,
-    city: "서울",
-    pet: "dog",
-  },
-  {
-    age: 36,
-    city: "서울",
-    pet: "dog",
-  },
-  {
-    age: 36,
-    city: "서울",
-    pet: "dog",
-  },
-  {
-    age: 36,
-    city: "서울",
-    pet: "dog",
-  },
-  {
-    age: 31,
-    city: "대구",
-    pet: "dog",
-  },
-  {
-    age: 16,
-    city: "부산",
-    pet: "dog",
-  },
-];
-
-//나이 30 미만 사람들의 주거지 도시 구하기
-function solveAModern() {
-  const allCities = people
-    //30미만
-    .filter(({ age }) => age < 30)
-    //도시 구하기
-    .map(({ city }) => city);
-  // 중복제거 new Set
-  const set = new Set(allCities);
-  return Array.from(set);
+function returnPromiseForTimeout() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Math.random());
+    }, 1000);
+  });
 }
 
-console.log("solveAModern", solveAModern());
+returnPromiseForTimeout()
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  })
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  })
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  })
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  });
 
-//각 도시별로 개와 고양이를 키우는 사람의 수
-/** @typedef {Object.<string, Object.<string, number>>} PetsOfCities */
-function solveBModern() {
-  return (
-    people
-      .map(({ pet: petOrPets, city }) => {
-        //자료구조 바꾸기
-        const pets =
-          (typeof petOrPets === "string" ? [petOrPets] : petOrPets) || [];
-        return {
-          city,
-          pets,
-        };
-        // 형태 { city: '서울', pets: [ 'cat', 'dog' ] }
-      })
-      // 배열 병합함수 -.flat()
-      // flatMap은 flat, map 합친 것
-      //형태 [ '서울', 'cat' ], [ '서울', 'dog' ]
-      .flatMap(({ city, pets }) => pets.map((pet) => [city, pet]))
-      .reduce((/** @type {PetsOfCities} */ result, [city, pet]) => {
-        if (!city || !pet) {
-          return result;
-        }
-        return {
-          ...result,
-          [city]: {
-            ...result[city],
-            //수 구하기(이미 있을경우 || 초기값 0) + 1
-            [pet]: (result[city]?.[pet] || 0) + 1,
-          },
-        };
-      }, {})
-  );
-}
-
-console.log("solveBModern", solveBModern());
+returnPromiseForTimeout();
